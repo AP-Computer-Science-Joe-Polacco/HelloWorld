1f3139397882eba61aafbda9cb865597d1b8df8d
jpolacco <jpolacco@gmail.com>
2022-09-01 14:54:45 -0700

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Greeter here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Greeter{
+    
+    /**
+     * returns a greeting in the format:
+     * "Hello Joe!" 
+     * 
+     * @param name name of person to greet
+     * @return a greeting in the format "Hello Joe!" for a person with name "Joe"
+     */
+    public String getGreeting(String name){
+        // TODO: your code here
+        return ""; // delete this line of code
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class HelloWorld
+{
+    public static void main(String[] args){
+        Greeter mom = new Greeter(); // instantiate a Greeter object
+        System.out.println(mom.getGreeting("Joe"));
+        // Write at least two more tests (using print statements) below
+    }
+}
